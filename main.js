18d397fb44fe44e9ef15250f5814a93074343e3d
JenGates3255 <Jengates3255@gmail.com>
2014-07-07 12:33:17 -0600

    first commit

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-$(document).on('ready', function() {
-  
-});
+var FoodItem = function (name, calories, vegan, glutenFree, citrusFree){
+  	this.name = name ;
+  	this.calories = calories;
+  	this.vegan = vegan;
+  	this.glutenFree = glutenFree;
+  	this.citrusFree = citrusFree;
+  	this.toString = function(){
+ 		console.log("Name:" + this.name + "\n" + "Calories:"  + this.calories + "\n" + "Vegan:" + this.vegan + "\n" + "GlutenFree:" + this.glutenFree + "\n" + "CitrusFree:" + this.citrusFree);
+  	}
+  }
+console.log(FoodItem);
+ 
+ var tortilla = new FoodItem('tortilla', 100, true, false, true);
+ console.log(tortilla) 
+
+
+
+
